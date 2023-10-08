a48d7509607fca01f7f2196fdbdbaabbb56587ef
Allan Santana <allanms12@gmail.com>
2023-10-08 15:40:26 -0300

    feat: Added printf fc in stage2 bootloader

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ The code is following the playlist below from nanobyte:
 [Link for the playlist](https://www.youtube.com/playlist?list=PLFjM7v6KGMpiH2G-kT781ByCNC_0pKpPN)
 
 The current OS is able to print error messages on screen, read the second part of bootloader from disk and run it!
-The second part of bootloader can also print on the screen using the 10h interrupt of BIOS
+The second part of bootloader can also print on the screen using an basic implementation of printf function in 10h interrupt of BIOS
 
 ## Languages used
  * Assembly
--- src/bootloader/stage2/x86.asm
+++ src/bootloader/stage2/x86.asm
@@ -23,3 +23,33 @@ _x86_Video_WriteCharTeletype:
     mov sp, bp
     pop bp
     ret
+
+global _x86_div64_32
+_x86_div64_32:
+    push bp
+    mov bp, sp
+
+    push bx
+
+    mov eax, [bp + 8]   ; eax <- Upper 32 bits of dividend
+    mov ecx, [bp + 12]  ; ecx <- Divisor
+    xor edx, edx
+    div ecx             ; eax <- quot, edx <- remainder
+    
+    mov bx, [bp + 16]
+    mov [bx + 4], eax
+
+    mov eax, [bp + 4]   ; eax <- Lower 32 bits of dividend
+                        ; edx <- Old remainder
+    div ecx
+
+    mov [bx], eax
+    mov bx, [bp + 18]
+    mov [bx], edx
+
+    pop bx
+
+    mov sp, bp
+    pop bp
+    ret
+
